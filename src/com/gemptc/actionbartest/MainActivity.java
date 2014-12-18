58c011ca9e3e9d99431dce361cbe592369c25af0
xibeiguyue <2118356023@qq.com>
2014-12-18 09:00:17 +0800

    shoucitijiao

---
+++ src/com/gemptc/actionbartest/MainActivity.java
@@ -0,0 +1,23 @@
+package com.gemptc.actionbartest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
